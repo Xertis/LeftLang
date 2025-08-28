7a988b0e6c996fb4cd39044f2afcdaee414ccdbc
Xertis <118364459+Xertis@users.noreply.github.com>
2025-08-28 16:11:17 +0300

    Тест коммит

--- src/main/kotlin/scripts/utils/fsm.kt
+++ src/main/kotlin/scripts/utils/fsm.kt
@@ -4,6 +4,10 @@ import lexer.TokenizerStates
 data class State(val state: TokenizerStates)
 
 class Fsm {
-    var middlewares = mutableListOf<() -> Unit>()
+    var middlewares = mutableListOf<(State) -> State?>()
     var state = State(TokenizerStates.DEFAULT)
+
+    fun addMiddleware(state: TokenizerStates, middleware: (State) -> State?) {
+
+    }
 }
